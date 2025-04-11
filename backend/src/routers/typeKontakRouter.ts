3aa0bd6346f4507ae7f5ad393a629a2f80727836
extra100 <bohhari224@gmail.com>
2025-04-11 04:20:45 +0000

    212

--- backend/src/routers/typeKontakRouter.ts
+++ backend/src/routers/typeKontakRouter.ts
@@ -2,64 +2,62 @@ import express, { Request, Response } from 'express'
 import asyncHandler from 'express-async-handler'
 import { TypeKontakModel } from '../models/typeKontakModel'
 
-export const typeKontakRouter = express.Router()
+const typeKontakRouter = express.Router()
 
 typeKontakRouter.get(
   '/',
   asyncHandler(async (req: Request, res: Response) => {
-    const bebas = await TypeKontakModel.find({})
-    res.json(bebas)
+    const semua = await TypeKontakModel.find({})
+    res.json(semua)
   })
 )
 
 typeKontakRouter.get(
-  '/:die',
+  '/:id',
   asyncHandler(async (req: Request, res: Response) => {
-    const angene = await TypeKontakModel.findById(req.params.die)
-    if (angene) {
-      res.json(angene)
+    const data = await TypeKontakModel.findById(req.params.id)
+    if (data) {
+      res.json(data)
     } else {
-      res.status(404).json({ message: 'Satuan not found' })
+      res.status(404).json({ message: 'TypeKontak not found' })
     }
   })
 )
 
 typeKontakRouter.post(
   '/',
-  asyncHandler(async (req, res) => {
-    const beudoang = req.body
-    delete beudoang._id
-    const justSatuan = await TypeKontakModel.create(beudoang)
-    res.status(201).json(justSatuan)
+  asyncHandler(async (req: Request, res: Response) => {
+    const newData = req.body
+    delete newData._id
+    const created = await TypeKontakModel.create(newData)
+    res.status(201).json(created)
   })
 )
+
 typeKontakRouter.put(
-  '/:eid',
+  '/:id',
   asyncHandler(async (req: Request, res: Response) => {
     const { type_kontak } = req.body
+    const data = await TypeKontakModel.findById(req.params.id)
 
-    const onlyHereSsatuan = await TypeKontakModel.findById(req.params.eid)
-
-    if (onlyHereSsatuan) {
-      onlyHereSsatuan.type_kontak = type_kontak || onlyHereSsatuan.type_kontak
-
-      const updatedSatuan = await onlyHereSsatuan.save()
-      res.json(updatedSatuan)
+    if (data) {
+      data.type_kontak = type_kontak || data.type_kontak
+      const updated = await data.save()
+      res.json(updated)
     } else {
-      res.status(404).json({ message: 'Satuan not found' })
+      res.status(404).json({ message: 'TypeKontak not found' })
     }
   })
 )
 
-
 typeKontakRouter.delete(
-  '/:idol',
-  asyncHandler(async (req, res) => {
-    const teneDwang = await TypeKontakModel.findByIdAndDelete(req.params.idol)
-    if (teneDwang) {
-      res.json({ message: 'Satuan deleted successfully' })
+  '/:id',
+  asyncHandler(async (req: Request, res: Response) => {
+    const deleted = await TypeKontakModel.findByIdAndDelete(req.params.id)
+    if (deleted) {
+      res.json({ message: 'TypeKontak deleted successfully' })
     } else {
-      res.status(404).json({ message: 'Satuan Not Found' })
+      res.status(404).json({ message: 'TypeKontak not found' })
     }
   })
 )
